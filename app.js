bdaa474bf33c8dde4cae104593ee36655f960144
Mr Hasnain <153563473+Mr-hasnain0001@users.noreply.github.com>
2024-05-04 17:47:47 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+function yellow() {
+    document.body.style.backgroundColor="yellow"
+}
+function pink() {
+    document.body.style.backgroundColor="pink"
+}
+function red() {
+    document.body.style.backgroundColor="red"
+}
+function green() {
+    document.body.style.backgroundColor="green"
+}
+function blue() {
+    document.body.style.backgroundColor="blue"
+}
+function add() {
+ document.getElementById("zero").innerHTML++;
+    
+}
+function min() {
+    document.getElementById("zero").innerHTML--;
+}
